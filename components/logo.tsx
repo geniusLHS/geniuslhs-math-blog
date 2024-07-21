a52f1c23f0df2e1703527e63196b546c43658e59
Hyunseo Lee <cube040318@gmail.com>
2024-07-21 18:44:02 +0900

    feat: 기본 설정 및 메인 페이지 작성

---
+++ components/logo.tsx
@@ -0,0 +1,56 @@
+const Logo = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="40.168px"
+    height="22.200px"
+    viewBox="0 -883.9 1998.1 894.9"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    style={{
+      border: "1px solid currentColor",
+      padding: "3px 2px",
+      borderRadius: "3px",
+    }}
+    {...props}
+  >
+    <defs>
+      <path
+        id="MJX-12-TEX-N-50"
+        d="M130 622Q123 629 119 631T103 634T60 637H27V683H214Q237 683 276 683T331 684Q419 684 471 671T567 616Q624 563 624 489Q624 421 573 372T451 307Q429 302 328 301H234V181Q234 62 237 58Q245 47 304 46H337V0H326Q305 3 182 3Q47 3 38 0H27V46H60Q102 47 111 49T130 61V622ZM507 488Q507 514 506 528T500 564T483 597T450 620T397 635Q385 637 307 637H286Q237 637 234 628Q231 624 231 483V342H302H339Q390 342 423 349T481 382Q507 411 507 488Z"
+      />
+      <path
+        id="MJX-12-TEX-N-32"
+        d="M109 429Q82 429 66 447T50 491Q50 562 103 614T235 666Q326 666 387 610T449 465Q449 422 429 383T381 315T301 241Q265 210 201 149L142 93L218 92Q375 92 385 97Q392 99 409 186V189H449V186Q448 183 436 95T421 3V0H50V19V31Q50 38 56 46T86 81Q115 113 136 137Q145 147 170 174T204 211T233 244T261 278T284 308T305 340T320 369T333 401T340 431T343 464Q343 527 309 573T212 619Q179 619 154 602T119 569T109 550Q109 549 114 549Q132 549 151 535T170 489Q170 464 154 447T109 429Z"
+      />
+      <path
+        id="MJX-12-TEX-N-65"
+        d="M28 218Q28 273 48 318T98 391T163 433T229 448Q282 448 320 430T378 380T406 316T415 245Q415 238 408 231H126V216Q126 68 226 36Q246 30 270 30Q312 30 342 62Q359 79 369 104L379 128Q382 131 395 131H398Q415 131 415 121Q415 117 412 108Q393 53 349 21T250 -11Q155 -11 92 58T28 218ZM333 275Q322 403 238 411H236Q228 411 220 410T195 402T166 381T143 340T127 274V267H333V275Z"
+      />
+    </defs>
+    <g stroke="currentColor" fill="currentColor" strokeWidth={0} transform="scale(1,-1)">
+      <g data-mml-node="math">
+        <g data-mml-node="TeXAtom" data-mjx-texclass="ORD">
+          <g data-mml-node="TeXAtom" data-mjx-texclass="ORD">
+            <g data-mml-node="msup">
+              <g data-mml-node="mi">
+                <use data-c={50} xlinkHref="#MJX-12-TEX-N-50" />
+              </g>
+              <g data-mml-node="mn" transform="translate(714,413) scale(0.707)">
+                <use data-c={32} xlinkHref="#MJX-12-TEX-N-32" />
+              </g>
+            </g>
+            <g data-mml-node="msup" transform="translate(1117.6,0)">
+              <g data-mml-node="mi">
+                <use data-c={65} xlinkHref="#MJX-12-TEX-N-65" />
+              </g>
+              <g data-mml-node="mn" transform="translate(477,413) scale(0.707)">
+                <use data-c={32} xlinkHref="#MJX-12-TEX-N-32" />
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default Logo;
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,57 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import Logo from "./components/logo";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – P^2e^2",
+    };
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+  logo: (
+    <div className="nx-flex nx-items-center">
+      <Logo className="nx-mr-2" />
+      페페의 수학탐험일지
+    </div>
+  ),
+  head: () => {
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+    const url = "https://math.geniuslhs.com" + asPath;
 
-export default config
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || "P^2e^2"} />
+        <meta property="og:description" content={frontMatter.description || "페페의 수학탐험일지"} />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </>
+    );
+  },
+  project: {
+    link: "https://github.com/geniusLHS/geniuslhs-math-blog",
+  },
+  search: {
+    emptyResult: <span className="nx-block nx-select-none nx-p-8 nx-text-center nx-text-sm nx-text-gray-400">검색 결과 없음</span>,
+    loading: <span className="nx-block nx-select-none nx-p-8 nx-text-center nx-text-sm nx-text-gray-400">로딩중...</span>,
+    error: "에러 발생",
+    placeholder: "검색",
+  },
+  docsRepositoryBase: "https://github.com/geniusLHS/geniuslhs-math-blog",
+  toc: {
+    title: "목차",
+    backToTop: true,
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: "오류 제보 및 피드백",
+  },
+  footer: {
+    text: "© 2024 geniusLHS",
+  },
+};
+
+export default config;
